625f354fd0edf41f1f49118ab6103b9f007e6ac2
Mike Sokolov <miha13.sokolov@gmail.com>
2024-03-10 17:26:12 +0500

    final

--- src/main/java/com/hailmik/lorbyproject/controller/LorbyController.java
+++ src/main/java/com/hailmik/lorbyproject/controller/LorbyController.java
@@ -1,5 +1,6 @@
 package com.hailmik.lorbyproject.controller;
 
+import com.hailmik.lorbyproject.dto.ConfirmationDTO;
 import com.hailmik.lorbyproject.dto.RegistrationDTO;
 import com.hailmik.lorbyproject.dto.UserDTO;
 import com.hailmik.lorbyproject.entity.User;
@@ -20,23 +21,23 @@ import java.util.Random;
 public class LorbyController {
 	private final LorbyService lorbyService;
 	private final EmailService emailService;
-	@GetMapping("/check")
-	public String check() {
-		return "Check success";
-	}
+//	@GetMapping("/check")
+//	public String check() {
+//		return "Check success";
+//	}
 
-	@GetMapping("/user/{email}")
-	public User getUser(@PathVariable String email) {
-		return lorbyService.findByEmail(email);
-	}
-	@GetMapping("/users")
-	public List<User> getUsers() {
-		return lorbyService.findAll();
-	}
-	@DeleteMapping("/delete/{email}")
-	public User deleteUser(@PathVariable String email) {
-		return lorbyService.deleteByEmail(email);
-	}
+//	@GetMapping("/user/{email}")
+//	public User getUser(@PathVariable String email) {
+//		return lorbyService.findByEmail(email);
+//	}
+//	@GetMapping("/users")
+//	public List<User> getUsers() {
+//		return lorbyService.findAll();
+//	}
+//	@DeleteMapping("/delete/{email}")
+//	public User deleteUser(@PathVariable String email) {
+//		return lorbyService.deleteByEmail(email);
+//	}
 
 	@PostMapping("/login")
 	public ResponseEntity<String> login(@RequestBody UserDTO user) {
@@ -55,14 +56,16 @@ public class LorbyController {
 	}
 
 	@PostMapping("/code")
-	public ResponseEntity<String> confirmCode(@RequestParam String code, @RequestParam String email) {
+	public ResponseEntity<Object> confirmCode(@RequestBody ConfirmationDTO confirmation) {
+		String code = confirmation.getCode();
+		String email = confirmation.getEmail();
 		User user = lorbyService.findByEmail(email);
 		System.out.println(user);
 		LocalDateTime current = LocalDateTime.now();
-		ResponseEntity<String> response;
+		ResponseEntity<Object> response;
 		if (current.minusMinutes(15).isBefore(user.getCodeReceived())) {
 			if (code.equals(user.getGeneratedCode())) {
-				response = new ResponseEntity<>("Success", HttpStatus.OK);
+				response = new ResponseEntity<>(user, HttpStatus.OK);
 				user.setEnabled(true);
 				lorbyService.saveUser(user);
 			} else {
